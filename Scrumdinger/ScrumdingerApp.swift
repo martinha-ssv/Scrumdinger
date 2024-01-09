cd6e0963707ffdebff43b36ea0c4def5d87090c0
martinha-ssv <marta.sofia.vasconcelos@gmail.com>
2024-01-09 22:54:40 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Marta Sofia da Silva Vasconcelos on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
