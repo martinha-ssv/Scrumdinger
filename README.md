d2c11f39a428da293740b401db185fc89c29d900
martinha-ssv <marta.sofia.vasconcelos@gmail.com>
2024-01-10 17:52:02 +0000

    Is this okay?

---
+++ README.md
@@ -0,0 +1,1 @@
+Currently learning how to build an IOS app by following an [Apple Dev Tutorial](https://developer.apple.com/tutorials/app-dev-training)!
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,17 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Marta Sofia da Silva Vasconcelos on 10/01/2024.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+            HStack {
+                configuration.title
+                configuration.icon
+            }
+        }
+}
